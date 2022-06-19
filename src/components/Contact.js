af3e3aa829f51525f6e3766d7311661739b7570b
rzkts <rizkituls212@gmail.com>
2022-06-19 18:29:37 +0700

    Challenge Intro React membuat dan menampilkan contact list

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,24 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+import "./Contact.css";
+
+const Contact = ({ data }) => {
+  return (
+    <>
+      <div className="container-contacts">
+        <div className="profile-img">
+          <img src={data.photo} alt="img-profile" className="picture"></img>
+        </div>
+        <div className="profile">
+          <h4 className="profile-name">{data.name}</h4>
+          <p className="profile-phone">{data.phone}</p>
+          <p className="profile-email">{data.email}</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <>
+      <div className="container-header">
+        <h1 className="title">Call a Friend</h1>
+        <p className="subtitle">your friendly contact app</p>
+        <p className="divider">
+          **************************************************************************************************
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default Header;
